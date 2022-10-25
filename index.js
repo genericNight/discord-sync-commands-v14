707064799fa2028d8f67a1420d9b7fffe41055be
Cosmos <110777803+iscosmos@users.noreply.github.com>
2022-10-25 09:51:50 -0700

    client optimizations

--- index.js
+++ index.js
@@ -9,24 +9,14 @@ module.exports = async (client, commands, options = {
     const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
     await ready;
     const currentCommands = await client.application.commands.fetch(options.guildId && { guildId: options.guildId });
-
-    log('\x1b[33m%s\x1b[0m', `Syncing commands with Discord...`);
-    log('\x1b[33m%s\x1b[0m', `Running ${currentCommands.size} commands.`);
-
     const newCommands = commands.filter((command) => !currentCommands.some((c) => c.name === command.name));
     for (let newCommand of newCommands) {
         await client.application.commands.create(newCommand, options.guildId);
     }
-
-    log('\x1b[32m%s\x1b[0m', `Created ${newCommands.length} commands.`);
-
     const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name)).toJSON();
     for (let deletedCommand of deletedCommands) {
         await deletedCommand.delete();
     }
-
-    log(`\x1b[31m%s\x1b[0m Deleted ${deletedCommands.length} commands!`);
-
     const updatedCommands = commands.filter((command) => currentCommands.some((c) => c.name === command.name));
     let updatedCommandCount = 0;
     for (let updatedCommand of updatedCommands) {
@@ -40,11 +30,6 @@ module.exports = async (client, commands, options = {
             updatedCommandCount++;
         }
     }
-
-    log('\x1b[33m%s\x1b[0m', `Updated ${updatedCommandCount} commands!`);
-
-    log(`Commands synchronized!`);
-
     return {
         currentCommandCount: currentCommands.size,
         newCommandCount: newCommands.length,
